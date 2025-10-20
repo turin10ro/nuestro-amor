f90595ca9c065689ffd510640736da71d96e2f46
turin10ro <miguelarturo.alcantara.1a@gmail.com>
2025-10-19 18:06:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ mensajeDinamico.classList.add('latido');
 // 📸 Generar galería de fotos automáticamente
 const galeria = document.getElementById('galeria');
 
-for (let i = 1; i <= 89; i++) {
+for (let i = 1; i <= 110; i++) {
   const img = document.createElement('img');
   img.src = `images/foto${i}.jpeg`;
   img.alt = `Foto ${i} de nosotros`;
