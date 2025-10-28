69833e8653cca3dedae6aed2ab33b6da08c976f0
turin10ro <miguelarturo.alcantara.1a@gmail.com>
2025-10-27 20:35:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ mensajeDinamico.classList.add('latido');
 // 📸 Generar galería de fotos automáticamente
 const galeria = document.getElementById('galeria');
 
-for (let i = 1; i <= 110; i++) {
+for (let i = 1; i <= 121; i++) {
   const img = document.createElement('img');
   img.src = `images/foto${i}.jpeg`;
   img.alt = `Foto ${i} de nosotros`;
@@ -89,3 +89,4 @@ const observer = new IntersectionObserver((entries) => {
 fotos.forEach(foto => {
   observer.observe(foto);
 });
+
